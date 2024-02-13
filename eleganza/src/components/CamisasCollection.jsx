267ba350c65716b6f2e40f387a4480bdb47bcaee
Matu36 <matipineda85@live.com.ar>
2024-02-13 17:35:28 -0300

    freakstyle

--- eleganza/src/components/CamisasCollection.jsx
+++ eleganza/src/components/CamisasCollection.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { GrClose } from "react-icons/gr";
+import Camisani from "../assets/img/sweater.png";
+import Camisani2 from "../assets/img/swater2.png";
+import freak from "../assets/img/freak.png";
+import freak2 from "../assets/img/freak1.png";
+import moño from "../assets/img/moño.png";
+import moño1 from "../assets/img/moño1.png";
 
 export default function CamisasCollection({ handleCerrarModalCamisas }) {
   return (
-    <div>
-      <button onClick={handleCerrarModalCamisas} className="button-cerrar">
-        <GrClose />
-      </button>
-      Holissss
+    <div className="colleccionremerascontainer">
+      <div className="remerasCollection">
+        <div className="imagen1">
+          <img src={freak} alt="" />
+        </div>
+        <div className="imagen1">
+          <img src={freak2} alt="" />
+        </div>
+      </div>
+
+      <div className="button-cerrar-remeras">
+        <button
+          onClick={handleCerrarModalCamisas}
+          className="button-cerrar-remeras"
+        >
+          <GrClose />
+        </button>
+      </div>
     </div>
   );
 }
